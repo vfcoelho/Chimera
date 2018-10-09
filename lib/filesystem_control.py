74d4e64db91e0820ccf628e4c3a064617b2f42b4
vfcoelho <coelho182@outlook.com>
2018-10-09 03:17:26 -0300

    finished file management features and added logger to all functions

--- lib/filesystem_control.py
+++ lib/filesystem_control.py
@@ -4,57 +4,65 @@ from pathlib import Path
 
 class FileSystemControl:
     
-    _CURRENT_PATH = 'current_path'
+    _CURRENT_PATH = '_current_path'
     
-    current_path = ""
+    _current_path = ""
     
-    @memory
-    async def __init__(self,memory,initial_path=None):
-        
-        await memory_path = memory.get(self._CURRENT_PATH)
+    def __init__(self,initial_path=None):
         
         if initial_path:
             path = Path(initial_path)
-        elif memory_path:
-            path = Path(memory_path)
         else:
             path = Path.cwd()
         
-        if not path.exists():
-            raise Exception('Provided path does not exists!')
+        path = self._check_path(path)
              
-        self.current_path = path
-        
-    @memory
-    async def set_path(self, memory, path,relative=False):
-        new_path = Path(path) if not relative else Path(str(self.current_path),path)
-        if not new_path.exists():
-            raise Exception('Provided path does not exists!')
-        await memory.set(self._CURRENT_PATH,str(new_path))
-        
-    @memory
-    async def move_up(self, memory, times = 0):
-        new_path = self.current_path.parents[times]
-        await memory.set(self._CURRENT_PATH,str(new_path))
+        self._current_path = path
     
-    def list_directory(self):
-        dir_path = self.current_path
+    def _check_path(self,path):
+        
+        path = path.resolve()
+        
+        if not path.exists():
+            raise Exception('Path [{}] does not exists!'.format(path))
+        
+        return path
+    
+    @memory
+    async def load_path_from_memory(self,memory):
+        memory_path = await memory.get(self._CURRENT_PATH)
+        if memory_path:
+            self._current_path = memory_path
+    
+    @memory
+    async def set_path(self, path, relative=False,memory=None):
+        new_path = Path(path) if not relative else Path(str(self._current_path),path)
+        new_path = self._check_path(new_path)
+        await memory.set(self._CURRENT_PATH,new_path)
+        return new_path
+    
+    async def list_directory(self):
+        dir_path = self._current_path
         if not dir_path.is_dir():
             dir_path = Path(*dir_path.parts[:-1])
-        return [x for x in self.current_path.iterdir()]
+        
+        return [x for x in self._current_path.iterdir()]
+        
     
-    def retrieve_file(self, file_name=None):
-        if self.current_path.is_file():
-            return self.current_path.read_bytes()
+    async def retrieve_file(self, file_name=None):
+        if self._current_path.is_file():
+            return self._current_path
         else:
             if file_name:
-                file_path = Path(self.current_path,file_name)
+                file_path = Path(self._current_path,file_name)
                 if file_path.is_file():
-                    return file_path.read_bytes()
+                    return file_path
                 
         raise Exception('No file specified')
     
-    def save_file(self,file_bytes,file_name):
-        file_path = Path(self.current_path,file_name)
-        file_path.write_bytes(file_bytes)
+    async def save_file(self,file,file_name,file_path=None):
+        file_path = file_path if file_path else self._current_path 
+        path = Path(file_path,file_name)
+        path.write_bytes(file)
+        return path
     
